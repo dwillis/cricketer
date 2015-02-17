36106cc1f29e50f238dc41181eefef5ecb0c31a9
Caleb <caleb.tutty@nzherald.co.nz>
2015-02-17 17:24:37 +1300

    Refactored out duplication in spec

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -4,20 +4,18 @@ RSpec.describe Cricketer::Match do
   subject { Cricketer::Match }
   let(:match_id) { 656399 }
 
-  it 'requests a URL when new is called' do
+  let(:match) do
     VCR.use_cassette("match_#{match_id}") do
-      match = subject.new match_id
-      url = match.instance_variable_get(:@json_url)
-      expect(url).to eq "http://www.espncricinfo.com/matches/engine/match/#{match_id}.json"
+      subject.new match_id
     end
   end
 
+  it 'requests a URL when new is called' do
+    url = match.instance_variable_get(:@json_url)
+    expect(url).to eq "http://www.espncricinfo.com/matches/engine/match/#{match_id}.json"
+  end
+
   context 'produces valid data' do
-    let(:match) do
-      VCR.use_cassette("match_#{match_id}") do
-        subject.new match_id
-      end
-    end
 
     it 'describes the match with #to_s' do
       expect(match.to_s).to eq 'ICC Cricket World Cup, 1st Match, Pool A: New Zealand v Sri Lanka at Christchurch, Feb 14, 2015'
