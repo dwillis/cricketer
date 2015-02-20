58ea83f031072bc20d6729a9340b17cf1c0694b8
Derek Willis <dwillis@nytimes.com>
2015-02-20 16:56:31 -0500

    sorted world cup points

--- lib/cricketer.rb
+++ lib/cricketer.rb
@@ -45,10 +45,10 @@ module Cricketer
 
   def self.worldcup_points
     url = "http://www.espncricinfo.com/wc2007/engine/series/509587.json?view=pointstable"
-    JSON.parse(open(url).read)
+    points = JSON.parse(open(url).read)
     pool_a = points['graph']["Pool A"].keys.map{|k| {id: k, name: points['graph']["Pool A"][k]['team_name'], matches: points['graph']["Pool A"][k]['points'].size, points: points['graph']["Pool A"][k]['points'].last['points']}}
     pool_b = points['graph']["Pool B"].keys.map{|k| {id: k, name: points['graph']["Pool B"][k]['team_name'], matches: points['graph']["Pool B"][k]['points'].size, points: points['graph']["Pool B"][k]['points'].last['points']}}
-    [pool_a, pool_b]
+    [pool_a.sort_by!{|h| h[:points]}.reverse, pool_b.sort_by!{|h| h[:points]}.reverse]
   end
 
 end
