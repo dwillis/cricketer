9289cb6afea02045894c46832586f2b1e00b7e36
Derek Willis <dwillis@nytimes.com>
2015-02-24 13:39:28 -0500

    world cup points table

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -1,7 +1,7 @@
 module Cricketer
   class WorldCup
 
-    def self.worldcup_points
+    def self.points_table
       url = "http://www.espncricinfo.com/wc2007/engine/series/509587.json?view=pointstable"
       points = JSON.parse(open(url).read)
       pool_a = points['graph']["Pool A"].keys.map{|k| {id: k, name: points['graph']["Pool A"][k]['team_name'], matches: points['graph']["Pool A"][k]['points'].size, points: points['graph']["Pool A"][k]['points'].last['points']}}
